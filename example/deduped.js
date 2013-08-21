9dd128e074b8af4c1fa7abc2d48cdf59b1291788
Thorsten Lorenz <thlorenz@gmx.de>
2013-08-21 14:20:13 -0400

    adding examples

---
+++ example/deduped.js
@@ -0,0 +1,8 @@
+var dedupe = require('../');
+dedupe.activate();
+
+var foo1 = require('./pack1/dep-uno/foo');
+var foo2 = require('./pack2/dep-uno/foo');
+
+console.log(foo1.foo);
+console.log(foo2.foo);
---
+++ example/not-deduped.js
@@ -0,0 +1,5 @@
+var foo1 = require('./pack1/dep-uno/foo');
+var foo2 = require('./pack2/dep-uno/foo');
+
+console.log(foo1.foo);
+console.log(foo2.foo);
